4f0b9b62a4c061bee4132e254dd9be95b1af5d41
MarceloMSilveira <marcelomendes.silveira@gmail.com>
2025-07-18 21:23:41 -0300

    start point

--- src/openMS1_frontend/src/App.jsx
+++ src/openMS1_frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css"; //dúvida sobre a existência desse bootstrap?
+//import "bootstrap/dist/css/bootstrap.min.css"; //dúvida sobre a existência desse bootstrap?
 import homeImage from "../public/home-img.png";
 
 function App() {
